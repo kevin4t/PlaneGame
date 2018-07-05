6f12fd830cd720d5d7514a923673f2792890ee3a
kevin4t <328795012@qq.com>
2018-07-05 21:25:08 +0800

    v0.0.2
    1.添加了Me类
    2.修改了窗体大小及位置

--- PlaneGame/Me.cpp
+++ PlaneGame/Me.cpp
@@ -18,7 +18,10 @@ CImageList CMe::m_Images;
 
 BOOL CMe::Draw(CDC* pDC, BOOL bPause)
 {
-	m_ptPos.x = m_ptPos.x + m_nHorMotion * 5;
+	m_ptPos.x = m_ptPos.x + m_nHorMotion * 10;
+	m_ptPos.y = m_ptPos.y + m_nVerMotion * 10;
+	m_nHorMotion = 0;
+	m_nVerMotion = 0;
 
 	return m_Images.Draw(pDC, 0, m_ptPos, ILD_TRANSPARENT);
 }
